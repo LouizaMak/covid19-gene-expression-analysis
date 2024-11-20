60e6b378b9f70d2d0968bff67c62de3b8dd5af68
Louiza Mak <louiza.mak@gmail.com>
2024-11-20 14:47:14 -0600

    refactor and reassign gene labels to df

--- gene_expression_pipeline.py
+++ gene_expression_pipeline.py
@@ -3,24 +3,27 @@ import math
 from pydeseq2.dds import DeseqDataSet
 from pydeseq2.ds import DeseqStats
 
-def normalize_data_frame():
-
-    #read tsv file as pandas data frame
+def create_data_frame():
+    
+    # read tsv file as pandas data frame
     raw_df = pd.read_table('GSE147507_RawReadCounts_Human.tsv')
 
-    #remove df columns that do not follow A549_Mock/A549_Covid convention
+    raw_df = raw_df.rename(columns={'Unnamed: 0': 'Gene_Labels'})
+
+    # remove df columns that do not follow A549_Mock/A549_Covid convention
     remove_columns = ["ACE2", "NHBE", "Calu3", "HPIV", "Lung", "Series4", "Series3"]
     for substring in remove_columns:
         raw_df = raw_df.drop(columns=[col for col in raw_df.columns if substring in col])
 
-    #remove gene label column for data processing
+    # extract gene label column for reinsertion after running differential analysis
+    gene_labels = raw_df['Gene_Labels']
+
+    # remove gene label column for data processing
     filtered_df = raw_df.iloc[:, 1:].replace(0, 1)
 
     metadata = create_metadata(filtered_df)
 
-    # normalized_df.insert(0, 'Unnamed: 0', raw_df['Unnamed: 0'])
-
-    return filtered_df, metadata
+    return filtered_df, metadata, gene_labels
 
 def create_metadata(normalized_df):
 
@@ -39,29 +42,37 @@ def create_metadata(normalized_df):
 
     return metadata
 
-def run_differential_analysis(df, metadata):
+def run_differential_analysis(df, metadata, gene_labels):
     dds = DeseqDataSet(
         counts=df.T,
         metadata=metadata,
         design_factors="condition"
     )
 
-    #DESeq2 normalization and dispersion estimation of data
+    # DESeq2 normalization and dispersion estimation of data
     dds.deseq2()
 
-    #covid_treated vs covid_untreated
+    # covid_treated vs covid_untreated
     covid_stats = DeseqStats(dds, contrast=["condition", "covid-treated", "covid-untreated"])
 
-    #identify differentially expressed genes statistically
+    # identify differentially expressed genes statistically
     covid_stats.summary()
 
-    #set appropriate significance thresholds (e.g., adjusted p-value < 0.05)
-    stats_df = covid_stats.results_df[covid_stats.results_df['pvalue'] < 0.05]
-    stats_df = stats_df[stats_df['padj'] < 0.05]
+    stats_df = covid_stats.results_df
 
-    #sort by ascending pvalue and keep first 100 rows
+    # reset indices for alignment
+    gene_labels = gene_labels.reset_index(drop=True)
+    stats_df = stats_df.reset_index(drop=True)
+
+    # reassign gene labels to df
+    stats_df.insert(0, gene_labels.name, gene_labels)
+
+    # set appropriate significance thresholds (e.g., adjusted p-value < 0.05)
+    stats_df = stats_df[(stats_df['pvalue'] < 0.05) & (stats_df['padj'] < 0.05)]
+
+    # sort by ascending pvalue and keep first 100 rows
     stats_df = stats_df.sort_values(by='pvalue').iloc[:100]
 
-    #reassign gene labels to 100 rows
+    print(stats_df.to_string())
 
-run_differential_analysis(*normalize_data_frame())
+run_differential_analysis(*create_data_frame())
